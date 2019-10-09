dd7aab386d4b2df54651e82b02481dbbb1961549
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-10-09 17:15:15 +0200

    Improve initialiation of board

--- chess/init.lua
+++ chess/init.lua
@@ -78,40 +78,6 @@ function love.load()
         for j=1,8,1 do
             pos = files[j]..ranks[i]
             positions[pos] = {center_x=cen_x, center_y=cen_y, x=x, y=y, file=j, rank=i}
-            if pos == "e8" then positions[pos].piece = {color="black", name="king", asset=bking, kind="king"}
-            elseif pos == "d8" then positions[pos].piece = {color="black", name="queen", asset=bqueen, kind="queen"}
-            elseif pos == "a8" then positions[pos].piece = {color="black", name="rook1", asset=brook, kind="rook"}
-            elseif pos == "h8" then positions[pos].piece = {color="black", name="rook2", asset=brook, kind="rook"}
-            elseif pos == "c8" then positions[pos].piece = {color="black", name="bishop1", asset=bbishop, kind="bishop"}
-            elseif pos == "f8" then positions[pos].piece = {color="black", name="bishop2", asset=bbishop, kind="bishop"}
-            elseif pos == "g8" then positions[pos].piece = {color="black", name="knight1", asset=bknight, kind="knight"}
-            elseif pos == "b8" then positions[pos].piece = {color="black", name="knight2", asset=bknight, kind="knight"}
-            elseif pos == "a7" then positions[pos].piece = {color="black", name="pawn1", asset=bpawn, kind="pawn"}
-            elseif pos == "b7" then positions[pos].piece = {color="black", name="pawn2", asset=bpawn, kind="pawn"}
-            elseif pos == "c7" then positions[pos].piece = {color="black", name="pawn3", asset=bpawn, kind="pawn"}
-            elseif pos == "d7" then positions[pos].piece = {color="black", name="pawn4", asset=bpawn, kind="pawn"}
-            elseif pos == "e7" then positions[pos].piece = {color="black", name="pawn5", asset=bpawn, kind="pawn"}
-            elseif pos == "f7" then positions[pos].piece = {color="black", name="pawn6", asset=bpawn, kind="pawn"}
-            elseif pos == "g7" then positions[pos].piece = {color="black", name="pawn7", asset=bpawn, kind="pawn"}
-            elseif pos == "h7" then positions[pos].piece = {color="black", name="pawn8", asset=bpawn, kind="pawn"}
-            elseif pos == "e1" then positions[pos].piece = {color="white", name="king", asset=wking, kind="king"}
-            elseif pos == "d1" then positions[pos].piece = {color="white", name="queen", asset=wqueen, kind="queen"}
-            elseif pos == "a1" then positions[pos].piece = {color="white", name="rook1", asset=wrook, kind="rook"}
-            elseif pos == "h1" then positions[pos].piece = {color="white", name="rook2", asset=wrook, kind="rook"}
-            elseif pos == "c1" then positions[pos].piece = {color="white", name="bishop1", asset=wbishop, kind="bishop"}
-            elseif pos == "f1" then positions[pos].piece = {color="white", name="bishop2", asset=wbishop, kind="bishop"}
-            elseif pos == "g1" then positions[pos].piece = {color="white", name="knight1", asset=wknight, kind="knight"}
-            elseif pos == "b1" then positions[pos].piece = {color="white", name="knight2", asset=wknight, kind="knight"}
-            elseif pos == "a2" then positions[pos].piece = {color="white", name="pawn1", asset=wpawn, kind="pawn"}
-            elseif pos == "b2" then positions[pos].piece = {color="white", name="pawn2", asset=wpawn, kind="pawn"}
-            elseif pos == "c2" then positions[pos].piece = {color="white", name="pawn3", asset=wpawn, kind="pawn"}
-            elseif pos == "d2" then positions[pos].piece = {color="white", name="pawn4", asset=wpawn, kind="pawn"}
-            elseif pos == "e2" then positions[pos].piece = {color="white", name="pawn5", asset=wpawn, kind="pawn"}
-            elseif pos == "f2" then positions[pos].piece = {color="white", name="pawn6", asset=wpawn, kind="pawn"}
-            elseif pos == "g2" then positions[pos].piece = {color="white", name="pawn7", asset=wpawn, kind="pawn"}
-            elseif pos == "h2" then positions[pos].piece = {color="white", name="pawn8", asset=wpawn, kind="pawn"}
-            end
-
             cen_x = cen_x + 96
             x = x + 96
         end
@@ -121,6 +87,39 @@ function love.load()
         y = y + 96
     end
 
+    positions["e8"].piece = {color="black", name="king", asset=bking, kind="king"}
+    positions["d8"].piece = {color="black", name="queen", asset=bqueen, kind="queen"}
+    positions["a8"].piece = {color="black", name="rook1", asset=brook, kind="rook"}
+    positions["h8"].piece = {color="black", name="rook2", asset=brook, kind="rook"}
+    positions["c8"].piece = {color="black", name="bishop1", asset=bbishop, kind="bishop"}
+    positions["f8"].piece = {color="black", name="bishop2", asset=bbishop, kind="bishop"}
+    positions["g8"].piece = {color="black", name="knight1", asset=bknight, kind="knight"}
+    positions["b8"].piece = {color="black", name="knight2", asset=bknight, kind="knight"}
+    positions["a7"].piece = {color="black", name="pawn1", asset=bpawn, kind="pawn"}
+    positions["b7"].piece = {color="black", name="pawn2", asset=bpawn, kind="pawn"}
+    positions["c7"].piece = {color="black", name="pawn3", asset=bpawn, kind="pawn"}
+    positions["d7"].piece = {color="black", name="pawn4", asset=bpawn, kind="pawn"}
+    positions["e7"].piece = {color="black", name="pawn5", asset=bpawn, kind="pawn"}
+    positions["f7"].piece = {color="black", name="pawn6", asset=bpawn, kind="pawn"}
+    positions["g7"].piece = {color="black", name="pawn7", asset=bpawn, kind="pawn"}
+    positions["h7"].piece = {color="black", name="pawn8", asset=bpawn, kind="pawn"}
+    positions["e1"].piece = {color="white", name="king", asset=wking, kind="king"}
+    positions["d1"].piece = {color="white", name="queen", asset=wqueen, kind="queen"}
+    positions["a1"].piece = {color="white", name="rook1", asset=wrook, kind="rook"}
+    positions["h1"].piece = {color="white", name="rook2", asset=wrook, kind="rook"}
+    positions["c1"].piece = {color="white", name="bishop1", asset=wbishop, kind="bishop"}
+    positions["f1"].piece = {color="white", name="bishop2", asset=wbishop, kind="bishop"}
+    positions["g1"].piece = {color="white", name="knight1", asset=wknight, kind="knight"}
+    positions["b1"].piece = {color="white", name="knight2", asset=wknight, kind="knight"}
+    positions["a2"].piece = {color="white", name="pawn1", asset=wpawn, kind="pawn"}
+    positions["b2"].piece = {color="white", name="pawn2", asset=wpawn, kind="pawn"}
+    positions["c2"].piece = {color="white", name="pawn3", asset=wpawn, kind="pawn"}
+    positions["d2"].piece = {color="white", name="pawn4", asset=wpawn, kind="pawn"}
+    positions["e2"].piece = {color="white", name="pawn5", asset=wpawn, kind="pawn"}
+    positions["f2"].piece = {color="white", name="pawn6", asset=wpawn, kind="pawn"}
+    positions["g2"].piece = {color="white", name="pawn7", asset=wpawn, kind="pawn"}
+    positions["h2"].piece = {color="white", name="pawn8", asset=wpawn, kind="pawn"}
+    
 
     initMenu() 
 
